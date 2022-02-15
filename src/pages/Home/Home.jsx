8d8d2d3c2acd6be2e4b699b01dc75b43d5bc492a
altyntsevlexus <point-alex10@ukr.net>
2022-02-15 13:03:36 +0200

    fix: loader added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,20 @@
 import { useKolibriStateContext } from '../../contexts/kolibriContext';
+import { useBeaconStateContext } from '../../contexts/beaconContext';
 import Oven from '../../components/Oven';
 
 const Home = () => {
   const { myOvens } = useKolibriStateContext();
+  const { isLoggin } = useBeaconStateContext();
   return (
     <div>
-      {myOvens.map((ovenData) => {
-        return <Oven key={ovenData.ovenAddress} ovenData={ovenData} />;
-      })}
+      {isLoggin &&
+        (myOvens.length > 0 ? (
+          myOvens.map((ovenData) => {
+            return <Oven key={ovenData.ovenAddress} ovenData={ovenData} />;
+          })
+        ) : (
+          <p>Loading</p>
+        ))}
     </div>
   );
 };
