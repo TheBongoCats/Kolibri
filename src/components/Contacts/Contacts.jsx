f3cbecf0724dea32f2777127171b122bb8315c5d
Alexandr-Petrenko <alexandr.petrenko.fe@gmail.com>
2022-02-16 02:03:31 +0200

    feat: basic design features

---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,13 @@
+import Socials from '../Socials';
+import Policy from '../Policy';
+
+const Contacts = () => {
+  return (
+    <div>
+      <Policy />
+      <Socials />
+    </div>
+  );
+};
+
+export default Contacts;
