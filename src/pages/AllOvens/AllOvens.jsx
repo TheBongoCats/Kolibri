ad8162c969a5c5ac7a9c70042f6c203378d2993a
Alexandr-Petrenko <alexandr.petrenko.fe@gmail.com>
2022-02-18 01:11:36 +0200

    feat: ovens search was added

--- src/pages/AllOvens/AllOvens.jsx
+++ src/pages/AllOvens/AllOvens.jsx
@@ -11,10 +11,32 @@ const AllOvens = () => {
   const { allOvens, ovensWithBalance, tezosPrice } = useKolibriStateContext();
   const { getOvensWithBalance } = useKolibriDispatchContext();
   const [withBalance, setWithBalance] = useState(true);
-  const ovensForRender = withBalance ? [...ovensWithBalance] : [...allOvens];
+  const [searchedOvens, setSearchedOvens] = useState([]);
+  const [searchWasDone, setSearchWasDone] = useState(false);
+  const ovensForSearch = withBalance ? ovensWithBalance : allOvens;
+  const ovensForRender = searchWasDone ? searchedOvens : ovensForSearch;
+
   useEffect(async () => {
     await getOvensWithBalance();
   }, []);
+
+  const changeHandler = (e) => {
+    if (e.target.value.length >= 2) {
+      const arr = ovensForSearch.filter(
+        (oven) =>
+          oven.ovenOwner.includes(e.target.value) ||
+          oven.ovenAddress.includes(e.target.value),
+      );
+
+      setSearchedOvens(arr);
+    }
+
+    if (e.target.value.length > 2) {
+      setSearchWasDone(true);
+    } else {
+      setSearchWasDone(false);
+    }
+  };
 
   return (
     <div>
@@ -24,12 +46,12 @@ const AllOvens = () => {
         text={withBalance ? `Show empty ovens` : `Hide empty ovens`}
       />
 
-      <input type="text" />
+      <input type="text" onChange={(e) => changeHandler(e)} />
       <div>
         {tezosPrice ? (
-          ovensForRender.map((oven) => {
-            return <Oven ovenData={oven} key={oven.ovenAddress} />;
-          })
+          ovensForRender.map((oven) => (
+            <Oven ovenData={oven} key={oven.ovenAddress} />
+          ))
         ) : (
           <Loader />
         )}
