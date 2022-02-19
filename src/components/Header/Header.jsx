b61b22250951efd23fadfe911c8a65cbdfdf4d2b
Alexandr-Petrenko <alexandr.petrenko.fe@gmail.com>
2022-02-19 18:08:33 +0200

    feat: i18n was added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,23 +6,28 @@ import {
   useBeaconDispatchContext,
   useBeaconStateContext,
 } from '../../contexts/beaconContext';
+import I18n from '../I18n';
+import { buttonConnect, buttonDisconnect } from './texts';
+import { useI18nStateContext } from '../../contexts/i18nContext';
 
 const Header = () => {
   const { connectWallet, disconnectWallet } = useBeaconDispatchContext();
   const { isLogin } = useBeaconStateContext();
+  const { lang } = useI18nStateContext();
 
   return (
     <header className={styles.header}>
       <Logo />
       <Navigation />
       {isLogin ? (
-        <Button callback={disconnectWallet} text="Disconnect Wallet" />
+        <Button callback={disconnectWallet} text={buttonConnect[`${lang}`]} />
       ) : (
         <Button
           callback={() => connectWallet(true, 'hangzhounet')}
-          text="Connect Wallet"
+          text={buttonDisconnect[`${lang}`]}
         />
       )}
+      <I18n />
     </header>
   );
 };
