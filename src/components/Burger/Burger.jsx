c5f54b80d43ed4e61e06f1f7a951a7d8701ba890
Alexandr-Petrenko <alexandr.petrenko.fe@gmail.com>
2022-03-15 14:22:00 +0200

    feat(styling): safari styling and global extends file was added

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -3,14 +3,19 @@ import styles from './Burger.module.scss';
 
 const Burger = ({ isOpen, callback }) => {
   return (
-    <button
-      type="button"
-      className={
-        isOpen ? `${styles.burger} ${styles['burger--clicked']}` : styles.burger
-      }
-      onClick={callback}
-      aria-label="Open"
-    />
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
+    <div className={styles.wrapper} onClick={callback}>
+      <button
+        type="button"
+        className={
+          isOpen
+            ? `${styles.burger} ${styles['burger--clicked']}`
+            : styles.burger
+        }
+        aria-label="Open"
+        onClick={callback}
+      />
+    </div>
   );
 };
 
